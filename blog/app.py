c02a105a4697fe3587c67201468e8a54f15bf10a
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-04-24 11:51:28 +0300

    L3_0

--- blog/app.py
+++ blog/app.py
@@ -1,15 +1,48 @@
+from os import getenv, path
+
 from flask import Flask
+
+from json import load as js_load
 
 from blog.article.views import article
 from blog.user.views import user
 
+from blog.extension import db, login_manager
+# from blog.models import User
+
+
+CONFIG_PATH = getenv("CONFIG_PATH", path.join("../dev_config.json"))
+
+VIEWS = [
+    user,
+    article
+]
+
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config.from_file(CONFIG_PATH, load=js_load)
+    db.init_app(app)
+
+    from .models import User
+
+    # register_extensions(app)
+
     register_blueprint(app)
     return app
 
 
+def register_extensions(app):
+    db.init_app(app)
+    from blog.models import User
+    # login_manager.login_view = 'auth.login'
+    # login_manager.init_app(app)
+
+    # @login_manager.user_loader
+    # def load_user(user_id):
+    #     return User.query.get(int(user_id))
+
+
 def register_blueprint(app: Flask):
-    app.register_blueprint(user)
-    app.register_blueprint(article)
+    for view in VIEWS:
+        app.register_blueprint(view)

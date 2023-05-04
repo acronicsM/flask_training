f29ce96043404ac4d5f6aa79ac74e8cc64d52086
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-05-04 11:56:27 +0300

    L5

--- blog/auth/views.py
+++ blog/auth/views.py
@@ -62,25 +62,6 @@ def login():
     return render_template("auth/login.html", form=form, error=error)
 
 
-# @auth.route("/login", methods=["GET", "POST"])
-# def login():
-#     if request.method == "GET":
-#         return render_template(
-#             "auth/login.html"
-#         )
-#     from blog.models.user import User
-#
-#     email = request.form.get("email")
-#     password = request.form.get("password")
-#     user = User.query.filter_by(email=email).first()
-#
-#     if not user or not check_password_hash(user.password, password):
-#         flash("Check your login details")
-#         return redirect(url_for(".login"))
-#     login_user(user)
-#     return redirect(url_for("user.profile", pk=user.id))
-
-
 @auth.route("/logout")
 @login_required
 def logout():
